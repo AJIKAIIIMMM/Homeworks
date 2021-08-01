e02f98ccb97bcdeef6db3e102613ffadd37f41e1
AJIKAIIIMMM <maratmm31@gmail.com>
2021-08-01 16:17:47 +0300

    HomeworkObjects 6

--- GenericPlayer.cpp
+++ GenericPlayer.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdlib>
+#include <iomanip>
 
 using namespace std;
 
+
+
+
 class Card{
+public:
     enum suit{
         HEARTS,
         DIAMONDS,
         CLUBS,
         SPADES
-    }SuitsField;
+    };
     enum Rank{
         TWO = 2,
         THREE,
@@ -25,27 +31,39 @@ class Card{
         QUEEN = 10,
         KING = 10,
         ACE = 11
-    }CardsRank;
-    bool CardsPos;
-public:
+    };
     Card(suit a, Rank b, bool CardsPos = 0)
-    :SuitsField(a), CardsRank(b), CardsPos(CardsPos)
+    :m_Suit(a), m_Rank(b), CardsPos(CardsPos)
     {}
     void Flip(){
-        if(CardsPos == 0){
-            CardsPos = 1;
-            std::cout << "The card was fliped" << std::endl;
-        }else std::cout << "There is nothing to flip" << std::endl;
+    CardsPos = !(CardsPos);
     }
-    Rank GetValue()const {return CardsRank;}
+
+    int GetValue() const{
+        int value = 0;
+        if (CardsPos){
+            value = m_Rank;
+            if (value > 10){
+                value = 10;
+            }
+        }
+        return value;
+    }
+    friend ostream& operator<<(ostream& os, const Card& aCard);
+private:
+    suit m_Suit;
+    Rank m_Rank;
+    bool CardsPos;
 };
 
 class Hand{
+protected:
     vector<Card*> m_inHand;
 public:
     Hand(vector<Card*> inHand)
     :m_inHand(inHand){}
 
+    virtual ~Hand();
     void Add(Card* p){
         m_inHand.push_back(p);
     }
@@ -55,7 +73,7 @@ public:
         m_inHand.shrink_to_fit();
     }
 
-    int GetValue()const{
+    int GetTotal()const{
         auto iter = m_inHand.begin();
         int sum = 0;
         while(iter != m_inHand.end()){
@@ -79,18 +97,116 @@ public:
 };
 
 class GenericPlayer:public Hand{
-    string m_name;
+    friend ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer);
 public:
-    GenericPlayer(vector<Card*> inHand, string name)
-    :Hand (inHand), m_name(name)
-    {}
-    bool IsBoosted(){
-        if(GetValue() > 21) return 1;
+    GenericPlayer(const string& name = "");
+
+    virtual ~GenericPlayer();
+
+    virtual bool IsHitting() const = 0;
+
+    bool IsBoosted()const{
+        if(GetTotal() > 21) return 1;
         else return 0;}
-    void Bust(){
-        if(IsBoosted() == true) cout << "The hand of " << m_name << "is overflowed" << endl;
+
+    void Bust()const{
+        if(IsBoosted() == true) cout << m_name << " busts.\n";
+    }
+    string m_name;
+};
+
+ostream& operator<<(ostream& os, const Card& aCard)
+{
+    const string RANKS[] = { "0", "A", "2", "3", "4", "5", "6", "7", "8", "9","10", "J", "Q", "K" };
+    const string SUITS[] = { "c", "d", "h", "s" };
+
+    if (aCard.CardsPos)
+    {
+        os << RANKS[aCard.m_Rank] << SUITS[aCard.m_Suit];
+    }
+    else
+    {
+        os << "XX";
+    }
+
+    return os;
+}
+
+ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer)
+{
+    os << aGenericPlayer.m_name << ":\t";
+
+    vector<Card*>::const_iterator pCard;
+    if (!aGenericPlayer.m_inHand.empty())
+    {
+        for (pCard = aGenericPlayer.m_inHand.begin();
+             pCard != aGenericPlayer.m_inHand.end();
+             ++pCard)
+        {
+            os << *(*pCard) << "\t";
+        }
+
+
+        if (aGenericPlayer.GetTotal() != 0)
+        {
+            cout << "(" << aGenericPlayer.GetTotal() << ")";
+        }
+    }
+    else
+    {
+        os << "<empty>";
+    }
+
+    return os;
+}
+
+class Player : public GenericPlayer
+{
+public:
+    Player(const string& name = "");
+
+    virtual ~Player();
+
+    virtual bool IsHitting() const{
+        cout << m_name << ", do you want a hit? (Y/N): ";
+        char response;
+        cin >> response;
+        return (response == 'y' || response == 'Y');
+    }
+    void Win() const{
+        cout << m_name << " wins.\n";
+    }
+    void Lose() const{
+        cout << m_name << " loses.\n";
+    }
+    void Push() const{
+        cout << m_name << " pushes.\n";
     }
 };
+
+class House : public GenericPlayer
+{
+public:
+    House(const string& name = "House");
+
+    virtual ~House();
+
+    virtual bool IsHitting() const{
+        return (GetTotal() <= 16);
+    }
+    void FlipFirstCard(){
+        if (!(m_inHand.empty()))
+        {
+            m_inHand[0]->Flip();
+        }
+        else
+        {
+            cout << "No card to flip!\n";
+        }
+    }
+};
+
+
 
 int main(){
     return 0;
